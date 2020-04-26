1ca98b1b9ca1266f8851feddacb34259d53e6551
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-04-27 04:19:50 +0900

    fix ble.scan.start => ble.scan.startWiat and print on obniz text between 21s to 30s

--- srt.js.test/obnizTest.js
+++ srt.js.test/obnizTest.js
@@ -8,6 +8,7 @@ obniz.display.print("Obniz test");
 00:00:02,000 --> 00:00:07,000
 obniz.display.clear();
 obniz.display.print("ゆまち");
+console.log("ゆまち")
 
 2
 00:00:08,000 --> 00:00:20,000
@@ -17,11 +18,16 @@ obniz.switch.onchange = function (state) {
 };
 
 3
+00:00:21,000 --> 00:00:30,000
+obniz.display.clear();
+obniz.display.print("番台さんかっこいい");
+
+4
 00:00:40,000 --> 00:00:47,000
 (async function() {
     await obniz.ble.initWait();
     //start ble scan
-    obniz.ble.scan.start({ duration: 30 });
+    await obniz.ble.scan.startWait({ duration: 30 });
     obniz.ble.scan.onfind = function(peripheral){
         console.log(peripheral.localName)
         obniz.display.clear();
