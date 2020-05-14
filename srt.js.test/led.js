73c61727c9bbcbe7e8cc5355688602e9562fc6d7
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-05-15 03:01:31 +0900

    add led.js

---
+++ srt.js.test/led.js
@@ -0,0 +1,15 @@
+0
+00:00:00,000 --> 00:00:01,000
+doOnce[index] = true;
+obniz.display.clear();
+obniz.display.print("LED test");
+led = obniz.wired("LED", { anode:0, cathode:5 } );
+
+1
+00:00:24,000 --> 00:00:26,000
+player.pauseVideo(); // 動画の一時停止
+obniz.switch.onchange = function(state) {
+    if(state === "push") {
+        player.playVideo();
+    }
+}
