16ea3ec589cf2d0d1eccb1f7c84cca60e5d61e58
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-05-15 04:06:01 +0900

    turn on led

--- srt.js.test/led.js
+++ srt.js.test/led.js
@@ -12,11 +12,15 @@ player.pauseVideo(); // 動画の一時停止
 obniz.switch.onchange = function(state) {
     if(state === "push") {
         player.playVideo();
-    }
-    obniz.switch.onchange = function(state) {
-        console.log("上書き完了");
+        obniz.switch.onchange = function(state) {
+            console.log("上書き完了");
+        }
     }
 }
 
+1
+00:00:30,000 --> 00:00:31,000
+doOnce[index] = true;
+led.on();
 
 
