1bc3ff6ab07790a417651870cc7d563b6c4e4445
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-05-15 03:11:52 +0900

    improve

--- srt.js.test/led.js
+++ srt.js.test/led.js
@@ -7,6 +7,7 @@ led = obniz.wired("LED", { anode:0, cathode:5 } );
 
 1
 00:00:24,000 --> 00:00:26,000
+doOnce[index] = true;
 player.pauseVideo(); // 動画の一時停止
 obniz.switch.onchange = function(state) {
     if(state === "push") {
