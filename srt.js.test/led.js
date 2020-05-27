ee6ef6a747422c223a8108c0faefe8404a6fe3ef
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-05-27 13:01:45 +0900

    右ボタンを押すと次のシーンにシークする

--- srt.js.test/led.js
+++ srt.js.test/led.js
@@ -4,6 +4,30 @@ doOnce[index] = true;
 obniz.display.clear();
 obniz.display.print("LED TEST");
 led = obniz.wired("LED", { anode:0, cathode:5 } );
+vars.leftfunc = {};
+vars.rightfunc = {};
+vars.pushfunc = {};
+obniz.switch.onchange = function(state) {
+    if(state === "left") {
+        let fn = indexedFunction(vars.leftfunc);
+        if (fn != null) {
+            fn.call(null);
+            console.log(index);
+            console.log(fn);
+            console.log(vars.leftfunc)
+        }
+    } else if(state === "right") {
+        let fn = indexedFunction(vars.rightfunc);
+        if (fn != null) {
+            fn.call(null);
+        }
+    } else if(state === "push") {
+        let fn = indexedFunction(vars.pushfunc);
+        if(fn != null) {
+            fn.call(null);
+        }
+    }
+}
 const doc = editor.getDoc();
 const preValue = doc.getValue()
 doc.setValue(
@@ -16,18 +40,17 @@ obniz.display.print('LED TEST')`
 00:00:24,000 --> 00:00:26,000
 doOnce[index] = true;
 player.pauseVideo(); // 動画の一時停止
-obniz.switch.onchange = function(state) {
-    if(state === "push") {
-        player.playVideo();
-        obniz.switch.onchange = function(state) {
-            console.log("上書き完了");
-        }
-    }
+const currentTime = player.getCurrentTime()
+const seekTime = 29 - currentTime;
+vars.rightfunc[index] = function() {
+    player.seekTo(seekTime,true);
+}
+vars.pushfunc[index] = function() {
+    player.playVideo();
 }
 
 2
 00:00:29,500 --> 00:00:31,000
-doOnce[index] = true;
 led.on();
 const doc = editor.getDoc();
 doc.setValue(
@@ -37,6 +60,11 @@ obniz.onconnect = async function () {
     led.on()
 }`
 );
+const currentTime = player.getCurrentTime()
+const seekTime = 34 - currentTime;
+vars.rightfunc[index] = function() {
+    player.seekTo(seekTime,true);
+}
 
 3
 00:00:34,000 --> 00:00:34,500
