b11d129deefeed9143ca625e162dc324747456ad
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-06-11 02:32:39 +0900

    fix

--- srt.js.test/servo.js
+++ srt.js.test/servo.js
@@ -38,7 +38,7 @@ vars.rightfunc[index] = function() {
 servo.angle(90.0);
 
 3
-00:00:59,250 --> 00:01:22,000
+00:00:01,000 --> 00:01:22,000
 servo.angle(20.0)
 vars.leftfunc[index] = function() {
     servo.angle(20.0)
@@ -46,7 +46,7 @@ vars.leftfunc[index] = function() {
 vars.pushfunc[index] = function() {
     servo.angle(90.0)
 }
-vars.rightfunc[index] = function(){
+vars.rightfunc[index] = function() {
     servo.angle(180.0)
 }
 
