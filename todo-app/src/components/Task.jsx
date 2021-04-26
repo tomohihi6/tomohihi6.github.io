396d8087473586f11d5eb7b6784aeb743e8978e0
Tomohiro Kawatani <b1017111@fun.ac.jp>
2021-04-26 20:20:42 +0900

    fix: text decoration line if task is checked

--- todo-app/src/components/Task.jsx
+++ todo-app/src/components/Task.jsx
@@ -13,7 +13,9 @@ export const Task = ({ task }) => {
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <Checkbox checked={checked} color="primary" onChange={handleCheckChange} />
-      <div style={{ marginLeft: '6px' }}>{task.texts}</div>
+      <div style={checked ? { marginLeft: '6px', textDecorationLine: 'line-through' } : { marginLeft: '6px' }}>
+        {task.texts}
+      </div>
     </div>
   );
 };
