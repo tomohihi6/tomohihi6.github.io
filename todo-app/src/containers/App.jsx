fcebc6aeb081c677173270a427a0d8a30761bbfc
Tomohiro Kawatani <b1017111@fun.ac.jp>
2021-04-26 22:05:31 +0900

    fix: duplicated line

--- todo-app/src/containers/App.jsx
+++ todo-app/src/containers/App.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect, useCallback } from 'react';
-import React, { useState, useCallback } from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
